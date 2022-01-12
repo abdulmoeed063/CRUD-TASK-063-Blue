a47ac1b3638420e0c7759038e800470d1d122680
Abdul Moeez <Abdul Moeez@DESKTOP-UST9UJ2>
2022-01-12 12:49:24 +0500

    Add project files.

---
+++ AbdulMoeed063/DataDb/StoreDb.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AbdulMoeed063.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AbdulMoeed063.DataDb
+{
+    public class StoreDb : DbContext
+    {
+        public StoreDb(DbContextOptions<StoreDb> options) : base(options)
+        {
+
+        }
+        public DbSet<OrderModel> orders { get; set; }
+    }
+}
